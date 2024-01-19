c9b9bcc391debd5b7dcb5c33b02193ba9b77274f
vladradu <mevladradu@gmail.com>
2024-01-19 20:18:58 +0200

    week2 project structure

---
+++ cinema/src/main/java/com/nagarro/af24/cinema/CinemaApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.af24.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaApplication.class, args);
+	}
+
+}
