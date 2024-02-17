2f0c7128308ad288a9a945b91e2c7cb944e72055
vladradu <mevladradu@gmail.com>
2024-02-17 12:58:25 +0200

    week03: removed unused code 2

--- cinema/src/main/java/com/nagarro/af24/cinema/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.af24.cinema.repository;
-
-import com.nagarro.af24.cinema.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, Integer> {
-}
