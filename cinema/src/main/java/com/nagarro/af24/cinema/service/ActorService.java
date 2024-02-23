46b015a5ab65cde4ec2010bf25b4800de0956a3c
vladradu <mevladradu@gmail.com>
2024-02-23 17:25:29 +0200

    week04: removed service interface

--- cinema/src/main/java/com/nagarro/af24/cinema/service/ActorService.java
+++ cinema/src/main/java/com/nagarro/af24/cinema/service/ActorService.java
@@ -1,13 +1,44 @@
 package com.nagarro.af24.cinema.service;
 
 import com.nagarro.af24.cinema.dto.ActorDTO;
+import com.nagarro.af24.cinema.exception.CustomConflictException;
+import com.nagarro.af24.cinema.exception.CustomNotFoundException;
+import com.nagarro.af24.cinema.mapper.ActorMapper;
+import com.nagarro.af24.cinema.model.Actor;
+import com.nagarro.af24.cinema.repository.ActorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ActorService {
-    ActorDTO addActor(ActorDTO actorDTO);
+public class ActorService {
+    private final ActorRepository actorRepository;
+    private final ActorMapper actorMapper;
 
-    ActorDTO getActor(String name);
+    @Autowired
+    public ActorService(ActorRepository actorRepository, ActorMapper actorMapper) {
+        this.actorRepository = actorRepository;
+        this.actorMapper = actorMapper;
+    }
 
-    void deleteActor(String name);
+    public ActorDTO addActor(ActorDTO actorDTO) {
+        if (actorRepository.findByName(actorDTO.name()).isPresent()) {
+            throw new CustomConflictException("Actor already exists");
+        }
+
+        Actor actorToSave = actorMapper.toEntity(actorDTO);
+        Actor savedActor = actorRepository.save(actorToSave);
+        return actorMapper.toDTO(savedActor);
+    }
+
+    public ActorDTO getActor(String name) {
+        Actor actor = actorRepository.findByName(name)
+                .orElseThrow(() -> new CustomNotFoundException("Actor not found"));
+        return actorMapper.toDTO(actor);
+    }
+
+    public void deleteActor(String name) {
+        Actor actor = actorRepository.findByName(name)
+                .orElseThrow(() -> new CustomNotFoundException("Actor not found"));
+        actorRepository.delete(actor);
+    }
 }
--- cinema/src/main/java/com/nagarro/af24/cinema/service/MovieActorService.java
+++ cinema/src/main/java/com/nagarro/af24/cinema/service/MovieActorService.java
@@ -1,13 +1,54 @@
 package com.nagarro.af24.cinema.service;
 
+import com.nagarro.af24.cinema.dto.ActorDTO;
+import com.nagarro.af24.cinema.dto.MovieDTO;
 import com.nagarro.af24.cinema.dto.MovieDetailsDTO;
+import com.nagarro.af24.cinema.exception.CustomNotFoundException;
+import com.nagarro.af24.cinema.mapper.ActorMapper;
+import com.nagarro.af24.cinema.mapper.MovieMapper;
+import com.nagarro.af24.cinema.model.Actor;
+import com.nagarro.af24.cinema.model.Movie;
+import com.nagarro.af24.cinema.repository.ActorRepository;
+import com.nagarro.af24.cinema.repository.MovieRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface MovieActorService {
-    MovieDetailsDTO addActorToMovie(String movieTitle, List<String> actorsNames);
+public class MovieActorService {
+    private final MovieRepository movieRepository;
+    private final ActorRepository actorRepository;
+    private final MovieMapper movieMapper;
+    private final ActorMapper actorMapper;
 
-    MovieDetailsDTO getMovieDetails(String movieTitle);
+    public MovieActorService(MovieRepository movieRepository, ActorRepository actorRepository, MovieMapper movieMapper, ActorMapper actorMapper) {
+        this.movieRepository = movieRepository;
+        this.actorRepository = actorRepository;
+        this.movieMapper = movieMapper;
+        this.actorMapper = actorMapper;
+    }
+
+    public MovieDetailsDTO addActorToMovie(String movieTitle, List<String> actorsNames) {
+        Movie movie = movieRepository.findByTitle(movieTitle)
+                .orElseThrow(() -> new CustomNotFoundException("Movie not found"));
+
+        List<Actor> actors = actorRepository.findByNameIn(actorsNames);
+        List<Actor> existingActors = movie.getActors();
+        existingActors.addAll(actors);
+        movie.setActors(existingActors);
+
+        Movie savedMovie = movieRepository.save(movie);
+        MovieDTO movieDTO = movieMapper.toDTO(savedMovie);
+        List<ActorDTO> actorDTOS = actorMapper.toDTOs(savedMovie.getActors());
+        return new MovieDetailsDTO(movieDTO, actorDTOS);
+    }
+
+    public MovieDetailsDTO getMovieDetails(String movieTitle) {
+        Movie movie = movieRepository.findByTitle(movieTitle)
+                .orElseThrow(() -> new CustomNotFoundException("Movie not found"));
+
+        MovieDTO movieDTO = movieMapper.toDTO(movie);
+        List<ActorDTO> actorDTOS = actorMapper.toDTOs(movie.getActors());
+        return new MovieDetailsDTO(movieDTO, actorDTOS);
+    }
 }
