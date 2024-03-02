46aa36d0e3f25aec9acc44d2a969a4235cd70778
vladradu <mevladradu@gmail.com>
2024-03-02 15:48:32 +0200

    week05: misspell

--- cinema/src/test/java/com/nagarro/af24/cinema/repository/MovieRepositoryIntegrationTest.java
+++ cinema/src/test/java/com/nagarro/af24/cinema/repository/MovieRepositoryIntegrationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-class MovieRepositoryIntegrationIntegrationTest extends BaseRepositoryIntegrationTest {
+class MovieRepositoryIntegrationTest extends BaseRepositoryIntegrationTest {
     private Movie movieToSave;
     private Movie savedMovie;
 
