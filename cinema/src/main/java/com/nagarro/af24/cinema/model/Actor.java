b35d8f333b685be780edd9c20081a27790388340
vladradu <mevladradu@gmail.com>
2024-03-04 16:35:31 +0200

    week06: flyway init schema

--- cinema/src/main/java/com/nagarro/af24/cinema/model/Actor.java
+++ cinema/src/main/java/com/nagarro/af24/cinema/model/Actor.java
@@ -1,6 +1,8 @@
 package com.nagarro.af24.cinema.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -35,6 +37,7 @@ public class Actor {
     private int age;
 
     @NotNull
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
     @ManyToOne(fetch = FetchType.LAZY)
