560e6fc9caef744b83656d8261388a70801cc1e1
vladradu <mevladradu@gmail.com>
2024-03-09 15:44:12 +0200

    week07: added Controller Integration Tests

---
+++ cinema/src/test/java/com/nagarro/af24/cinema/controller/ReviewControllerIntegrationTest.java
@@ -0,0 +1,78 @@
+package com.nagarro.af24.cinema.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nagarro.af24.cinema.dto.MovieDTO;
+import com.nagarro.af24.cinema.dto.ReviewDTO;
+import com.nagarro.af24.cinema.utils.TestData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ReviewControllerIntegrationTest extends BaseControllerIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        MovieDTO movieDTO = TestData.getMovieDTO();
+        try {
+            mockMvc.perform(post("/movies")
+                            .contentType("application/json")
+                            .content(objectMapper.writeValueAsString(movieDTO)))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void addReview() throws Exception {
+        //Arrange
+        ReviewDTO reviewDTO = TestData.getReviewDTO();
+
+        //Act and Assert
+        mockMvc.perform(post("/reviews")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(reviewDTO)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getReviews() throws Exception {
+        //Arrange
+        List<ReviewDTO> reviewDTOS = TestData.getReviewDTOs();
+
+        reviewDTOS.forEach(reviewDTO -> {
+            try {
+                mockMvc.perform(post("/reviews")
+                                .contentType("application/json")
+                                .content(objectMapper.writeValueAsString(reviewDTO)))
+                        .andExpect(status().isOk());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        //Act
+        MvcResult mvcResult = mockMvc.perform(get("/reviews")
+                        .param("movieTitle", reviewDTOS.get(0).movieTitle())
+                        .param("movieProductionYear", String.valueOf(reviewDTOS.get(0).movieProductionYear())))
+                .andExpect(status().isOk())
+                .andReturn();
+        List<ReviewDTO> foundReviews = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), List.class);
+
+        //Assert
+        Assertions.assertEquals(reviewDTOS.size(), foundReviews.size());
+    }
+}
