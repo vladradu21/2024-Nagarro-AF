ad846cccd4fcfc9621b94bacbafe9e7db684f99b
vladradu <mevladradu@gmail.com>
2024-03-24 20:54:42 +0200

    week09: insert admin

--- cinema/src/main/java/com/nagarro/af24/cinema/CinemaApplication.java
+++ cinema/src/main/java/com/nagarro/af24/cinema/CinemaApplication.java
@@ -1,13 +1,36 @@
 package com.nagarro.af24.cinema;
 
+import com.nagarro.af24.cinema.model.ApplicationUser;
+import com.nagarro.af24.cinema.model.Role;
+import com.nagarro.af24.cinema.repository.RoleRepository;
+import com.nagarro.af24.cinema.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class CinemaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner run(RoleRepository roleRepository,
+                          UserRepository userRepository,
+                          PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isPresent()) {
+                return;
+            }
+            Set<Role> allRoles = new HashSet<>(roleRepository.findAll());
+            ApplicationUser admin = new ApplicationUser("admin", passwordEncoder.encode("parola123"), allRoles);
+            userRepository.save(admin);
+        };
+    }
 }
