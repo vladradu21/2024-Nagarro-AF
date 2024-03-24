9f55ab9159747613858065db03ec5226a733548f
vladradu <mevladradu@gmail.com>
2024-03-24 20:45:03 +0200

    week09: Repos

---
+++ cinema/src/main/java/com/nagarro/af24/cinema/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.nagarro.af24.cinema.repository;
+
+import com.nagarro.af24.cinema.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByAuthority(String authority);
+}
