7087bceb72ac96edd8d4790d3e9402801cbeb2e3
vladradu <mevladradu@gmail.com>
2024-03-24 20:43:06 +0200

    week09: ApplicationUser and Role

---
+++ cinema/src/main/java/com/nagarro/af24/cinema/model/Role.java
@@ -0,0 +1,50 @@
+package com.nagarro.af24.cinema.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    public Role(Long roleId, String authority) {
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public Long getRoleId() {
+        return this.roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+}
