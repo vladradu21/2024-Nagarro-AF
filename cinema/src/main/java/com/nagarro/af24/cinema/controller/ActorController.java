d6b6cf1700bd574a336c3ca283093372429efad8
vladradu <mevladradu@gmail.com>
2024-04-02 20:49:11 +0300

    week10: removed api interfaces

--- cinema/src/main/java/com/nagarro/af24/cinema/controller/ActorController.java
+++ cinema/src/main/java/com/nagarro/af24/cinema/controller/ActorController.java
@@ -3,8 +3,17 @@ package com.nagarro.af24.cinema.controller;
 import com.nagarro.af24.cinema.dto.ActorDTO;
 import com.nagarro.af24.cinema.dto.MovieDetailsDTO;
 import com.nagarro.af24.cinema.service.ActorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +23,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/actors")
-public class ActorController implements ActorApi{
+public class ActorController {
     private final ActorService actorService;
 
     @Autowired
@@ -22,27 +31,89 @@ public class ActorController implements ActorApi{
         this.actorService = actorService;
     }
 
-    @Override
+    @Operation(summary = "Add a new actor", description = "Add a new actor to the database", tags = {"Actors"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Actor added successfully",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ActorDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid input",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class)))
+            }
+    )
+    @PostMapping
     public ActorDTO addActor(@Valid @RequestBody ActorDTO actorDTO) {
         return actorService.addActor(actorDTO);
     }
 
-    @Override
+    @Operation(summary = "Get an actor", description = "Get an actor from the database", tags = {"Actors"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Actor found",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ActorDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid input",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class))),
+                    @ApiResponse(responseCode = "404", description = "Actor not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class)))
+            }
+    )
+    @GetMapping
     public ActorDTO getActor(@RequestParam String name) {
         return actorService.getActor(name);
     }
 
-    @Override
+    @Operation(summary = "Update an actor", description = "Update an actor in the database", tags = {"Actors"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Actor updated successfully",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ActorDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid input",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class))),
+                    @ApiResponse(responseCode = "404", description = "Actor not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class)))
+            }
+    )
+    @PutMapping
     public ActorDTO updateActor(@Valid @RequestBody ActorDTO actorDTO) {
         return actorService.updateActor(actorDTO);
     }
 
-    @Override
+    @Operation(summary = "Assign actors to a movie", description = "Assign actors to a movie in the database", tags = {"Actors"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Actors assigned successfully",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = MovieDetailsDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid input",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class))),
+                    @ApiResponse(responseCode = "404", description = "Movie not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class)))
+            }
+    )
+    @PutMapping("/assign-to-movie")
     public MovieDetailsDTO assignActorsToMovie(@RequestParam String movieTitle, @RequestParam int year, @RequestParam List<String> actorsNames) {
         return actorService.assignActorsToMovie(movieTitle, year, actorsNames);
     }
 
-    @Override
+    @Operation(summary = "Delete an actor", description = "Delete an actor from the database", tags = {"Actors"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Actor deleted successfully",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ActorDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid input",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class))),
+                    @ApiResponse(responseCode = "404", description = "Actor not found",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Error.class)))
+            }
+    )
+    @DeleteMapping
     public void deleteActor(@RequestParam String name) {
         actorService.deleteActor(name);
     }
